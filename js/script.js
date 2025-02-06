daf43be84e61ab24f1e6144b7677a3aa4be6e6fe
pauline muthoni <kinyanjuipauline69@gmail.com>
2025-02-07 00:28:09 +0300

    add js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+function showalert() {
+  alert("welcome, start shopping now");
+}
+
+const addToCartButtons = document.querySelectorAll(".btn-secondary");
+
+addToCartButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+
+    alert(" Add item to cart");
+  });
+});
+
+const newsletterForm = document.querySelector(".newsletter-form");
+newsletterForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = newsletterForm.querySelector('input[type="email"]');
+  const email = emailInput.value;
+
+  if (email) {
+    alert(`Thank you for subscribing with email: ${email}`);
+    emailInput.value = "";
+  } else {
+    alert("Please enter a valid email address.");
+  }
+});
