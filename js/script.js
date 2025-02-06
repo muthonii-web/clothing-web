20b63349b29875d0b33bd9c64ff50624c9e6d93b
pauline muthoni <kinyanjuipauline69@gmail.com>
2025-02-07 00:30:35 +0300

    change email alert

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ newsletterForm.addEventListener("submit", function (event) {
   const email = emailInput.value;
 
   if (email) {
-    alert(`Thank you for subscribing with email: ${email}`);
+    alert(`Thank you for subscribing.: ${email}`);
     emailInput.value = "";
   } else {
     alert("Please enter a valid email address.");
